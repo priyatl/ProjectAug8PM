63455941cf47fff7695685a9c637b5e7c6441eb4
priyatl <priyarasubsc@gmail.com>
2020-08-30 17:52:34 +0530

    Project work

--- src/test/java/Sample.java
+++ src/test/java/Sample.java
@@ -1,6 +1,7 @@
 
 public class Sample {
  public static void main(String[] args) {
-	System.out.println("project23");
+	 System.out.println("project23");
+	 System.out.println("project233333");
 }
 }
